041e2d356ba49b2738adf5a9e2168b833cc95c20
0xdomyz <yzdominic@outlook.com>
2022-02-06 02:20:44 +1100

    change config for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import pathlib
 import sys
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-
+import os
+#import pathlib
+#sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
