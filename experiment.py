b27b8520f8a487159ad69dd4a1565fef4ba43cdf
0xdomyz <yzdominic@outlook.com>
2023-07-10 22:46:19 +1000

    add to experiments

--- experiment.py
+++ experiment.py
@@ -23,7 +23,35 @@ triage_loads(b)
 
 
 # base64 string, even if string is a dict's json.dumps
+import base64
 
+
+def _encode(x: str) -> str:
+    """
+    Examples
+    -------------
+    ::
+
+        _encode("asdf3qrfds+asdf2://asdf+sdaf")
+    """
+    if x is not None:
+        return base64.b64encode(x.encode("UTF-8")).decode("UTF-8")
+
+
+def _decode(x):
+    """
+    Examples
+    -------------
+    ::
+
+        _decode("YXNkZjNxcmZkcythc2RmMjovL2FzZGYrc2RhZg==")
+    """
+
+    if x is not None:
+        return base64.b64decode(x.encode("UTF-8")).decode("UTF-8")
+
+
+triage_loads(_decode(_encode(a)))
 
 # save thick mode as well
 # option1: new save_url func that saves thick mode paths, additional param in save_url
