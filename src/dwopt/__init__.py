38fca0581789e527989689abcf1aaa512f9dfec4
0xdomyz <yzdominic@outlook.com>
2023-07-30 20:42:01 +1000

    update pre-ini of objects

--- src/dwopt/__init__.py
+++ src/dwopt/__init__.py
@@ -1,7 +1,30 @@
 from .dbo import db, Db, Pg, Lt, Oc
 from .set_up import save_url, make_eng, _get_url
 from .testing import make_test_tbl
+import logging
 
-pg = db(_get_url("pg"))
-lt = db(_get_url("lt"))
-oc = db(_get_url("oc"))
+_logger = logging.getLogger(__name__)
+
+try:
+    url, kwargs = _get_url("lt")
+    lt = db(url, **kwargs)
+except Exception as e:
+    msg = f"Failed to initialize default lt operator: {e}"
+    _logger.debug(msg)
+    lt = msg
+
+try:
+    url, kwargs = _get_url("pg")
+    pg = db(url, **kwargs)
+except Exception as e:
+    msg = f"Failed to initialize default lt operator: {e}"
+    _logger.debug(msg)
+    pg = msg
+
+try:
+    url, kwargs = _get_url("oc")
+    oc = db(url, **kwargs)
+except Exception as e:
+    msg = f"Failed to initialize default lt operator: {e}"
+    _logger.debug(msg)
+    oc = msg
